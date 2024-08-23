1db2a153aa7bfcfd61141e5bb5767c32244d3d44
YannFrab <yfrab@hotmail.fr>
2024-08-23 13:33:10 +0800

    feat: track evolution portoflio

--- utils/format_ideal_portfolio.py
+++ utils/format_ideal_portfolio.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from treelib import Tree
 
+from rich import print
+
 # TODO: Look for human errors when filling the csv
 # TODO: Return warning if the sum of the probabilities is not 100
 # TODO: Return warning if different probabilities are given to the same category
 
 
-def format_ideal_portfolio(portfolio_csv: str) -> tuple[pd.DataFrame, pd.DataFrame]:
+def format_ideal_portfolio(portfolio_csv: pd.DataFrame) -> pd.DataFrame:
     # Remove empty rows and columns
     portfolio_csv.dropna(axis=0, how="all", inplace=True)
     portfolio_csv.dropna(axis=1, how="all", inplace=True)
@@ -14,7 +16,7 @@ def format_ideal_portfolio(portfolio_csv: str) -> tuple[pd.DataFrame, pd.DataFra
     # Verify that columns do not have empty spaces at the beginning or end
     portfolio_csv.columns = portfolio_csv.columns.str.strip()
 
-    # Replace missing values for the probability fo the same category
+    # Replace missing values for the probability of the same category
     levels = [c for c in portfolio_csv.columns if c.startswith("L") and c[1:].isdigit()]
     portfolio_csv.fillna(
         {
@@ -26,25 +28,35 @@ def format_ideal_portfolio(portfolio_csv: str) -> tuple[pd.DataFrame, pd.DataFra
         inplace=True,
     )
 
-    # TODO: Force every category to have the sum of its probabiltiies to sum to 1
+    # TODO: Force every category to have its sum of probabilties equal to 1
 
-    # Calculate the overall importance of every postion
+    # Find path and leaf probabilities
+    portfolio_csv["tree_path"] = ""
     portfolio_csv["p_overall"] = 1.0
     for level in levels:
+        # Add level name to the path
+        portfolio_csv["tree_path"] += " - " + portfolio_csv[level].astype(str)
+
+        # Calculate the importance of each category
         non_nans = portfolio_csv[f"p_{level}"].notna()
         portfolio_csv.loc[non_nans, "p_overall"] *= (
             portfolio_csv.loc[non_nans, f"p_{level}"] / 100
         )
-    portfolio_csv["p_overall"] *= (1 / portfolio_csv["p_overall"].sum()) * 100
 
+    # Divide by the amount of identical leaves
+    portfolio_csv["tree_path"] = portfolio_csv.groupby("tree_path")[
+        "tree_path"
+    ].transform("count")
+    portfolio_csv["p_overall"] = portfolio_csv["p_overall"] / portfolio_csv["tree_path"]
+    portfolio_csv["p_overall"] *= (1 / portfolio_csv["p_overall"].sum()) * 100
+    portfolio_csv.drop(columns="tree_path", inplace=True)
     portfolio_csv = portfolio_csv.sort_values(by="p_overall", ascending=False)
     portfolio_csv.reset_index(drop=True, inplace=True)
-    print(
-        "Breakdown of each asset in the theoretical portfolio:\n",
-        portfolio_csv[["Product", "Tag", "p_overall"]],
-        "\n",
-    )
-    return portfolio_csv
+
+    # Return the breakdown of the portfolio
+    retrieve_tree_structure(portfolio_csv)
+
+    return
 
 
 def _name_node(category: str, percentage: float):
@@ -52,8 +64,11 @@ def _name_node(category: str, percentage: float):
 
 
 def retrieve_tree_structure(portfolio_csv: pd.DataFrame) -> pd.DataFrame:
+    # Intitialize the tree tha tsummarizes the portfolio
     tree = Tree()
     tree.create_node("Portfolio", "portfolio")  # root node
+
+    # Name of the different nodes in the tree
     levels = [c for c in portfolio_csv.columns if c.startswith("L") and c[1:].isdigit()]
 
     # Add the first layer to the root "portfolio"
@@ -80,8 +95,6 @@ def retrieve_tree_structure(portfolio_csv: pd.DataFrame) -> pd.DataFrame:
             tree.create_node(child_name, child_name, parent=parent_name)
 
     # Based on the last layer, add the Tags to the tree
-
-    # unique_combinations = portfolio_csv["Tag"]
     for tag in portfolio_csv["Tag"].unique():
         row_tag = portfolio_csv[portfolio_csv.Tag == tag]
         n_levels = len(
@@ -95,9 +108,11 @@ def retrieve_tree_structure(portfolio_csv: pd.DataFrame) -> pd.DataFrame:
         parent_name = _name_node(
             row_tag[f"L{n_levels}"].iloc[0], row_tag[f"p_L{n_levels}"].iloc[0]
         )
-        tree.create_node(tag, tag, parent=parent_name)
 
-    print("Representation of the portfolio structure as a tree:")
+        final_tag = f"{tag} - p={row_tag['p_overall'].iloc[0]}"
+        tree.create_node(final_tag, tag, parent=parent_name)
+
+    print("Structure of the theoretical portfolio:")
     print(tree.show(stdout=False))
 
     return tree
